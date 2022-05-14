12f98b48f50d553a8153e5bba0f667255e99af9c
ex0ticOne <rricardorb@gmail.com>
2022-05-13 22:42:48 -0300

    inclusao projecoes

--- DashboardInteligencia.R
+++ DashboardInteligencia.R
@@ -1,64 +1,48 @@
 
 
 #Prepara os dados
-source('PreparoDadosInteligencia.R')
+#source('PreparoDadosInteligencia.R')
 
 #UI
-cabecalho <- dashboardHeader(title = "Dashboard de Inteligência em Sinistros", titleWidth = "500")
+  estilo <- tags$head(tags$style(HTML(".shiny-output-error-validation {color: black;font-size: 30px; text-align: center; font-weight: bold;}")))
+
+  cabecalho <- dashboardHeader(title = "Dashboard de Inteligência em Sinistros", titleWidth = "500")
 
   lateral <- dashboardSidebar(
     sidebarMenu(uiOutput("selecao_ramo"), 
-                uiOutput("selecao_UF"), 
-                uiOutput("selecao_inicio_periodo"), 
-                uiOutput("selecao_fim_periodo"),
-                menuItem("Nacional por Ramo", tabName = "SINISTROS_BRASIL_RAMO"),
-                menuItem("Por UF e por Ramo", tabName = "SINISTROS_UF_RAMO")))
+                uiOutput("selecao_UF"),
+                menuItem("Projeções Brasil", tabName = "PROJECOES_NACIONAL"),
+                menuItem("Projeções por UF", tabName = "PROJECOES_UF")))
 
-  corpo <- dashboardBody(
-            tabItems(
-              tabItem(tabName = "SINISTROS_BRASIL_RAMO", 
-                      plotOutput("PLOT_SINISTROS_BRASIL_RAMO", height = "600")), 
-              tabItem(tabName = "SINISTROS_UF_RAMO", 
-                      plotOutput("PLOT_SINISTROS_UF_RAMO", height = "600"))) 
+  corpo <- dashboardBody(estilo,
+            tabItems( 
+              tabItem(tabName = "PROJECOES_NACIONAL",
+                      htmlOutput("PLOT_PROJECAO_NACIONAL")),
+              tabItem(tabName = "PROJECOES_UF", 
+                      htmlOutput("PLOT_PROJECAO_UF"))) 
         )
 
 ui <- dashboardPage(cabecalho, lateral, corpo, skin = "purple")
 
 server <- function(input, output) {
 
-  output$selecao_ramo <- renderUI(selectInput("selecao_ramo", label = "Selecione o ramo", choices = lista_ramos))
+  output$selecao_ramo <- renderUI(selectInput("selecao_ramo", label = "Selecione o ramo", choices = lista_ramos, selected = "Automóvel - Casco"))
   
   output$selecao_UF <- renderUI(selectInput("selecao_UF", label = "Selecione a UF", choices = lista_UF))
   
-  output$selecao_inicio_periodo <- renderUI(selectInput("selecao_inicio_periodo", label = "Início do Período", lista_periodos))
-
-  output$selecao_fim_periodo <- renderUI(selectInput("selecao_fim_periodo", label = "Fim do Período", lista_periodos))
+  output$PLOT_PROJECAO_NACIONAL <- renderUI(tags$iframe(seamless = "seamless", 
+                                                        src = paste0("RESULTADOS_NACIONAL/Projeção de Sinistralidade - ", input$selecao_ramo, " - Nacional.html"), 
+                                                        width = "100%", 
+                                                        height = "600"))
   
-  output$PLOT_SINISTROS_BRASIL_RAMO <- renderPlot({ggplot(SINISTROS_MES_RAMO_NACIONAL[SINISTROS_MES_RAMO_NACIONAL$ramos == input$selecao_ramo & 
-                                   SINISTROS_MES_RAMO_NACIONAL$damesano >= input$selecao_inicio_periodo & 
-                                   SINISTROS_MES_RAMO_NACIONAL$damesano <= input$selecao_fim_periodo, ], 
-                                   aes(x = damesano)) +
-                                      geom_smooth(aes(y = SinistrosDiretos), method = "loess") + 
-                                      labs(title = paste0("Curva de Sinistralidade - ", DADOS_RAMOS$nome_ramo[which(DADOS_RAMOS$coramo == input$selecao_ramo)]), 
-                                           x = "Linha Temporal", 
-                                           y = "Escala Logarítmica de Sinistralidade") +
-                                      theme_classic() +
-                                      theme(text = element_text(size = 20))
-                                      })
   
-  output$PLOT_SINISTROS_UF_RAMO <- renderPlot({ggplot(SINISTROS_MES_RAMO_UF[SINISTROS_MES_RAMO_UF$ramos == input$selecao_ramo & 
-                                                        SINISTROS_MES_RAMO_UF$UF == input$selecao_UF &
-                                                        SINISTROS_MES_RAMO_UF$damesano >= input$selecao_inicio_periodo & 
-                                                        SINISTROS_MES_RAMO_UF$damesano <= input$selecao_fim_periodo, ], 
-                                                        aes(x = damesano)) + 
-                                                          geom_smooth(aes(y = SinistrosDiretos), method = "loess") + 
-                                                          labs(title = paste0("Curva de Sinistralidade - ", DADOS_RAMOS$nome_ramo[which(DADOS_RAMOS$coramo == input$selecao_ramo)], " - ", input$selecao_UF), 
-                                                               x = "Linha Temporal", 
-                                                               y = "Escala Logarítmica de Sinistralidade") +
-                                                          theme_classic() +
-                                                          theme(text = element_text(size = 20))
-                                                          })
-    }
+  output$PLOT_PROJECAO_UF <- renderUI(tags$iframe(seamless = "seamless", 
+                                          src = paste0("RESULTADOS_UF/Projeção de Sinistralidade - ", input$selecao_ramo, " - ", input$selecao_UF, ".html"), 
+                                          width = "100%", 
+                                          height = "600"))
+  
+    
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- PreparoDadosInteligencia.R
+++ PreparoDadosInteligencia.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 library(shinydashboard)
 library(prophet)
+library(htmltools)
 library(readr)
 library(dplyr)
 library(tidyr)
@@ -11,8 +12,8 @@ options(scipen = 999999)
 
 #unzip('BaseCompleta.zip', files = 'SES_UF2.csv')
 
-#DADOS_UF_MES_CIA <- read_delim("SES_UF2.csv", delim = ";", 
-                     # escape_double = FALSE, trim_ws = TRUE,  )
+DADOS_UF_MES_CIA <- read_delim("SES_UF2.csv", delim = ";", 
+                    escape_double = FALSE, trim_ws = TRUE)
 
 DADOS_RAMOS <- read_delim("NOMES_RAMOS.CSV", 
                           delim = ",", escape_double = FALSE, 
@@ -24,7 +25,6 @@ DADOS_UF_MES_CIA$ramos <- as.numeric(DADOS_UF_MES_CIA$ramos)
 DADOS_RAMOS$coramo <- as.numeric(DADOS_RAMOS$coramo)
 
 DADOS_SINISTRO <- DADOS_UF_MES_CIA[, c("coenti", "damesano", "sin_dir", "UF", "ramos")]
-
 
 SINISTROS_MES_RAMO_UF <- DADOS_SINISTRO %>%
   group_by(damesano, ramos, UF) %>%
@@ -40,7 +40,7 @@ SINISTROS_MES_RAMO_NACIONAL <- DADOS_SINISTRO %>%
 SINISTROS_MES_RAMO_NACIONAL <- SINISTROS_MES_RAMO_NACIONAL[SINISTROS_MES_RAMO_NACIONAL$SinistrosDiretos > 0, ]
 SINISTROS_MES_RAMO_NACIONAL$SinistrosDiretos <- log10(SINISTROS_MES_RAMO_NACIONAL$SinistrosDiretos)
 
-lista_ramos <- data.frame(table(DADOS_RAMOS$coramo))
+lista_ramos <- data.frame(table(DADOS_RAMOS$nome_ramo))
 lista_ramos <- as.factor(lista_ramos$Var1[lista_ramos$Freq != ""])
 
 lista_UF <- data.frame(table(DADOS_SINISTRO$UF))
@@ -49,9 +49,3 @@ lista_UF <- as.factor(lista_UF$Var1[lista_UF$Freq != ""])
 lista_periodos <- data.frame(table(DADOS_SINISTRO$damesano))
 lista_periodos <- as.factor(lista_periodos$Var1[lista_periodos$Freq != ""])
 
-plot <- SINISTROS_MES_RAMO_UF[SINISTROS_MES_RAMO_UF$ramos == 114 & 
-                              SINISTROS_MES_RAMO_UF$UF == 'SP' & 
-                              SINISTROS_MES_RAMO_UF$damesano >= 202001 & SINISTROS_MES_RAMO_UF$damesano <= 202112, ] %>% 
-  ggplot(aes(x = damesano)) +
-  geom_line(aes(y = SinistrosDiretos)) + 
-  theme_classic()
