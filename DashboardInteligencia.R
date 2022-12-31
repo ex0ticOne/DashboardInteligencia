894f286a97a83a5d5cfebf3548f03957342475f7
ex0ticOne <rricardorb@gmail.com>
2022-12-30 21:17:38 -0300

    COMMIT

--- DashboardInteligencia.R
+++ DashboardInteligencia.R
@@ -2,165 +2,143 @@
 
 #Prepara os dados
 source('PreparoDadosInteligencia.R')
+source('ProphetPersonalizado.R')
 
 #UI
   estilo <- tags$head(tags$style(HTML(".shiny-output-error-validation {color: black;font-size: 30px; text-align: center; font-weight: bold;}")))
 
-  cabecalho <- dashboardHeader(title = "Dashboard de Inteligência", titleWidth = "300")
+  cabecalho <- dashboardHeader(title = "Dashboard de Inteligência em Mercado de Seguros", 
+                               titleWidth = "500")
 
-  lateral <- dashboardSidebar(img(src = 'logo_regula.png', height = 250, width = 300),
-    sidebarMenu(menuItem("Projeções de Consumo de Tickets", 
-                  uiOutput("selecao_corretora"),
-                  menuSubItem("Por Corretora", tabName = "PROJECAO_CONSUMO_CORRETORAS"), startExpanded = TRUE),
-                menuItem("Projeções de Sinistralidade",
-                  uiOutput("selecao_ramo"),
-                  menuSubItem("Nacional", tabName = "PROJECOES_NACIONAL"),
-                  uiOutput("selecao_UF_sinistralidade"),
-                  menuSubItem("Por UF", tabName = "PROJECOES_UF"), startExpanded = FALSE),
-                menuItem("Projeção de Abertura de Corretoras", 
-                        uiOutput("selecao_UF_corretoras"),
-                        menuSubItem("Por UF", tabName = "ABERTURA_CORRETORAS_UF"), startExpanded = FALSE),
-                menuItem("Projeção de Quantidade de Seguro Auto",
-                  uiOutput("selecao_cobertura"),
-                  uiOutput("selecao_UF_seguroauto"),
-                  menuSubItem("Por UF", tabName = "ACIONAMENTO_SEGURO_AUTO_UF"), 
-                  uiOutput("selecao_regiaotarifaria"),
-                  menuSubItem("Por Região de tarifação", tabName = "ACIONAMENTO_SEGURO_AUTO_REGIAO"), startExpanded = FALSE),
-                menuItem("Predição de Inadimplentes", 
-                         menuSubItem("Clientes atuais", tabName = "SCORE_INADIMPLENTES_INTERNO"),
-                         menuSubItem("No mercado", tabName = "SCORE_INADIMPLENTES_EXTERNO"), startExpanded = FALSE),
-                menuItem("Sentimento do Segurado", 
-                         menuSubItem("Resultados em lista", tabName = "DADOS_COMENTARIOS_CLASSIFICADOS"),
-                         menuSubItem("Resumo Compilado", tabName = "RESUMO_CLASSIFICACAO"),
-                         menuSubItem("Nuvem de Palavras Positivas", tabName = "NUVEM_PALAVRAS_POSITIVAS"),
-                         menuSubItem("Nuvem de Palavras Negativas", tabName = "NUVEM_PALAVRAS_NEGATIVAS"))), width = 300)
+  lateral <- dashboardSidebar(sidebarMenu(
+    uiOutput("selecao_periodo"),
+    menuItem("Projeções de Sinistralidade",
+             uiOutput("selecao_ramo"),
+             menuSubItem("Nacional",
+                         tabName = "PROJECOES_NACIONAL"),
+             uiOutput("selecao_UF_sinistralidade"),
+             menuSubItem("Por UF", 
+                         tabName = "PROJECOES_UF"), startExpanded = TRUE),
+    menuItem("Projeção de Abertura de Corretoras", 
+             uiOutput("selecao_UF_corretoras"),
+             menuSubItem("Por UF", 
+                         tabName = "ABERTURA_CORRETORAS_UF"), startExpanded = TRUE), 
+    width = 300), width = 300)
 
   corpo <- dashboardBody(estilo,
             tabItems(
-              tabItem(tabName = "PROJECAO_CONSUMO_CORRETORAS",
-                      fluidRow(htmlOutput("PLOT_CONSUMO_CORRETORAS"))),
               tabItem(tabName = "PROJECOES_NACIONAL",
-                      fluidRow(htmlOutput("PLOT_PROJECAO_NACIONAL"),
-                      tableOutput("DADOS_PROJECAO_NACIONAL"))),
+                      dygraphOutput("PLOT_PROJECAO_NACIONAL", height = 700)),
               tabItem(tabName = "PROJECOES_UF", 
-                      fluidRow(htmlOutput("PLOT_PROJECAO_UF"),
-                      tableOutput("DADOS_PROJECAO_UF"))),
+                      dygraphOutput("PLOT_PROJECAO_UF", height = 700)),
               tabItem(tabName = "ABERTURA_CORRETORAS_UF", 
-                      htmlOutput("PLOT_ABERTURA_CORRETORAS_UF")),
-              tabItem(tabName = "ACIONAMENTO_SEGURO_AUTO_UF", 
-                      htmlOutput("PLOT_ACIONAMENTO_SEGURO_AUTO_UF")),
-              tabItem(tabName = "ACIONAMENTO_SEGURO_AUTO_REGIAO", 
-                      htmlOutput("PLOT_ACIONAMENTO_SEGURO_AUTO_REGIAO")),
-              tabItem(tabName = "SCORE_INADIMPLENTES_INTERNO",
-                      box(dataTableOutput("DADOS_INADIMPLENTES_INTERNO"),
-                          title = "Quem são os possíveis inadimplentes dentro da Regula Sinistros?", 
-                          solidHeader = TRUE, 
-                          width = "100%")),
-              tabItem(tabName = "SCORE_INADIMPLENTES_EXTERNO",
-                      box(dataTableOutput("DADOS_INADIMPLENTES_EXTERNO"), 
-                          title = "Quem são os possíveis inadimplentes no mercado dos corretores?", 
-                          solidHeader = TRUE,
-                          width = "100%")),
-              tabItem(tabName = "DADOS_COMENTARIOS_CLASSIFICADOS",
-                      box(dataTableOutput("DADOS_COMENTARIOS_CLASSIFICADOS"), 
-                          title = "Qual o sentimento do segurado atendido pela Regula Sinistros?", 
-                          solidHeader = TRUE, 
-                          width = "100%")),
-              tabItem(tabName = "RESUMO_CLASSIFICACAO", 
-                      plotOutput("PLOT_RESUMO_CLASSIFICACAO", 
-                                 width = "100%", 
-                                 height = "800")),
-              tabItem(tabName = "NUVEM_PALAVRAS_POSITIVAS", 
-                      titlePanel("O que os segurados estão dizendo de POSITIVO sobre a Regula Sinistros?"), 
-                      htmlOutput("NUVEM_POSITIVA")),
-              tabItem(tabName = "NUVEM_PALAVRAS_NEGATIVAS", 
-                      titlePanel("O que os segurados estão dizendo de NEGATIVO sobre a Regula Sinistros?"),
-                      htmlOutput("NUVEM_NEGATIVA"))
-              )
-            ) 
+                      dygraphOutput("PLOT_ABERTURA_CORRETORAS_UF", height = 700))))
         
 
 ui <- dashboardPage(cabecalho, lateral, corpo, skin = "purple")
 
 server <- function(input, output) {
 
-  output$selecao_ramo <- renderUI(selectInput("selecao_ramo", label = "Selecione o ramo", choices = lista_ramos, selected = "Automóvel - Casco"))
+  output$selecao_ramo <- renderUI(selectInput("ramo_selecionado_sinistralidade", 
+                                              label = "Selecione o Ramo", 
+                                              choices = DADOS_RAMOS$nome_ramo))
   
-  output$selecao_UF_sinistralidade <- renderUI(selectInput("selecao_UF_sinistralidade", label = "Selecione a UF", choices = lista_UF))
+  output$selecao_periodo <- renderUI(noUiSliderInput("periodo_selecionado", 
+                                                     label = "Selecione a quantidade de anos para as projeções", 
+                                                     min = 1, 
+                                                     max = 50, 
+                                                     value = 2, 
+                                                     step = 1, 
+                                                     height = 200, 
+                                                     orientation = "vertical",
+                                                     direction = "rtl", 
+                                                     format = wNumbFormat(decimals = 0)))
   
-  output$selecao_UF_seguroauto <- renderUI(selectInput("selecao_UF_seguroauto", label = "Selecione a UF", choices = lista_UF))
+  output$selecao_UF_sinistralidade <- renderUI(selectInput("UF_selecionada_sinistralidade", 
+                                                           label = "Selecione a UF", 
+                                                           choices = c("AC", "AL", "AM", "BA", "CE",
+                                                                       "DF", "ES", "GO", "MA", "MG", 
+                                                                       "MS", "MT", "PA", "PB", "PE",
+                                                                       "PI", "PR", "RJ", "RN", "RO",
+                                                                       "RR","RS", "SC", "SE", "SP", 
+                                                                       "TO"), 
+                                                           selected = "SP"))
   
-  output$selecao_UF_corretoras <- renderUI(selectInput("selecao_UF_corretoras", label = "Selecione a UF", choices = lista_UF))
+  output$selecao_UF_corretoras <- renderUI(selectInput("UF_selecionada_corretora", 
+                                                       label = "Selecione a UF", 
+                                                       choices = c("AC", "AL", "AM", "BA", "CE",
+                                                                   "DF", "ES", "GO", "MA", "MG", 
+                                                                   "MS", "MT", "PA", "PB", "PE",
+                                                                   "PI", "PR", "RJ", "RN", "RO",
+                                                                   "RR","RS", "SC", "SE", "SP", 
+                                                                   "TO"), 
+                                                       selected = "SP"))
   
-  output$selecao_regiaotarifaria <- renderUI(selectInput("selecao_regiaotarifaria", label = "Selecione a Região", choices = lista_regioes))
-  
-  output$selecao_cobertura <- renderUI(selectInput("selecao_cobertura", label = "Selecione a Cobertura", choices = c("APP", "CASCO", "RCDM", "RCDP")))
-  
-  output$selecao_corretora <- renderUI(selectInput("selecao_corretora", label = "Selecione a Corretora", choices = lista_corretoras))
-  
-  output$PLOT_PROJECAO_NACIONAL <- renderUI(tags$iframe(seamless = "seamless", 
-                                                        src = paste0("PROJECOES_SINISTRALIDADE/RESULTADOS_NACIONAL/", DADOS_RAMOS$coramo[which(DADOS_RAMOS$nome_ramo == input$selecao_ramo)], " - Nacional.html"), 
-                                                        width = "100%", 
-                                                        height = "600"))
-  
-  
-  output$PLOT_PROJECAO_UF <- renderUI(tags$iframe(seamless = "seamless", 
-                                          src = paste0("PROJECOES_SINISTRALIDADE/RESULTADOS_UF/", DADOS_RAMOS$coramo[which(DADOS_RAMOS$nome_ramo == input$selecao_ramo)], " - ", input$selecao_UF_sinistralidade, ".html"), 
-                                          width = "100%", 
-                                          height = "600"))
-  
-  output$PLOT_ABERTURA_CORRETORAS_UF <- renderUI(tags$iframe(seamless = "seamless", 
-                                                  src = paste0("ABERTURA_CORRETORAS/Projeção de Abertura de Corretoras - ", input$selecao_UF_corretoras, ".html"), 
-                                                  width = "100%", 
-                                                  height = "600"))
-  
-  output$PLOT_ACIONAMENTO_SEGURO_AUTO_UF <- renderUI(tags$iframe(seamless = "seamless", 
-                                                  src = paste0("ACIONAMENTO_AUTO/UF/", input$selecao_cobertura, "/Projeção de Quantidade de Sinistros Seguro Auto - ", input$selecao_UF_seguroauto, " - ", input$selecao_cobertura, ".html"), 
-                                                  width = "100%", 
-                                                  height = "600"))
-  
-  output$PLOT_ACIONAMENTO_SEGURO_AUTO_REGIAO <- renderUI(tags$iframe(seamless = "seamless", 
-                                                                 src = paste0("ACIONAMENTO_AUTO/REGIAO/", input$selecao_cobertura, "/Projeção de Quantidade de Sinistros Seguro Auto - ", input$selecao_regiaotarifaria, " - ", input$selecao_cobertura, ".html"), 
-                                                                 width = "100%", 
-                                                                 height = "600"))
-  
-  output$PLOT_CONSUMO_CORRETORAS <- renderUI(tags$iframe(seamless = "seamless", 
-                                                         src = paste0("CONSUMO_CORRETORAS/", NOMES_CORRETORAS$cod_corretora[which(NOMES_CORRETORAS$nome_corretora == input$selecao_corretora)], " - Mensal.html"), 
-                                                         width = "100%", 
-                                                         height = "600"))
-  
-  output$PLOT_RESUMO_CLASSIFICACAO <- renderPlot(RESUMO_CLASSIFICACAO %>% 
-                                                         ggplot(aes(x = Resultado, y = Percentual, fill = Resultado)) + 
-                                                         geom_bar(stat = "identity") + 
-                                                         theme_classic() + 
-                                                         scale_fill_manual(values = c('Muito Negativa' = "darkred",
-                                                                                      'Muito Positiva' = "forestgreen",
-                                                                                      'Negativa' = "red",
-                                                                                      'Neutro' = "yellow",
-                                                                                      'Positiva' = "lightgreen",
-                                                                                      'Não Categorizado' = "grey")) +
-                                                         labs(title = "Percentual por Espectro", 
-                                                              x = "Espectros",
-                                                              y = "Percentual") + 
-                                                         geom_text(aes(label = Percentual, y = Percentual + 2)) + 
-                                                         theme(text = element_text(size = 20)))
-  
-  output$DADOS_COMENTARIOS_CLASSIFICADOS <- renderDataTable(COMENTARIOS_CLASSIFICADOS)
-  
-  output$NUVEM_POSITIVA <- renderUI(tags$iframe(seamless = "seamless", 
-                                                src = paste0("CLASSIFICADOR_NPS/nuvem_positiva.png"), 
-                                                width = "100%", 
-                                                height = "900"))
-  
-  output$NUVEM_NEGATIVA <- renderUI(tags$iframe(seamless = "seamless", 
-                                                src = paste0("CLASSIFICADOR_NPS/nuvem_negativa.png"), 
-                                                width = "100%", 
-                                                height = "900"))
-  
-  output$DADOS_INADIMPLENTES_INTERNO <- renderDataTable(TOP10_INADIMPLENTES_CLIENTES)
-  
-  output$DADOS_INADIMPLENTES_EXTERNO <- renderDataTable(POSSIVEIS_INADIMPLENTES_MERCADO)
+  renderiza_plot_nacional <- reactive({
     
+    modelo_sinistralidade <- read_rds(paste0(getwd(), "/modelos_nacional/", 
+                                             DADOS_RAMOS$coramo[DADOS_RAMOS$nome_ramo == input$ramo_selecionado_sinistralidade], 
+                                             ".rds"))
+    
+    periodo_futuro <- make_future_dataframe(modelo_sinistralidade, periods = input$periodo_selecionado * 12, freq = "month")
+    
+    projecao <- predict(modelo_sinistralidade, periodo_futuro)
+    
+    plot_sinistralidade <- dyplot.prophet(modelo_sinistralidade, projecao, uncertainty = TRUE, 
+                                          main = paste0(input$ramo_selecionado_sinistralidade, " -  Nacional"),
+                                          xlab = "Linha Temporal",
+                                          ylab = "Escala Logarítmica de Sinistralidade")
+    
+    return(plot_sinistralidade)
+    
+  })
+  
+  output$PLOT_PROJECAO_NACIONAL <- renderDygraph(renderiza_plot_nacional())
+  
+  renderiza_plot_UF <- reactive({
+    
+    modelo_sinistralidade <- read_rds(paste0(getwd(), "/modelos_uf/", 
+                                             DADOS_RAMOS$coramo[DADOS_RAMOS$nome_ramo == input$ramo_selecionado_sinistralidade], 
+                                             "-", 
+                                             input$UF_selecionada_sinistralidade,
+                                             ".rds"))
+    
+    periodo_futuro <- make_future_dataframe(modelo_sinistralidade, periods = input$periodo_selecionado * 12, freq = "month")
+    
+    projecao <- predict(modelo_sinistralidade, periodo_futuro)
+    
+    plot_sinistralidade <- dyplot.prophet(modelo_sinistralidade, projecao, uncertainty = TRUE, 
+                                          main = paste0(input$ramo_selecionado_sinistralidade, " - ", input$UF_selecionada_sinistralidade),
+                                          xlab = "Linha Temporal",
+                                          ylab = "Escala Logarítmica de Sinistralidade")
+    
+    return(plot_sinistralidade)
+    
+  })
+  
+  output$PLOT_PROJECAO_UF <- renderDygraph(renderiza_plot_UF())
+  
+  renderiza_plot_corretoras <- reactive({
+    
+    modelo_corretora <- read_rds(paste0(getwd(), "/modelos_corretoras/", 
+                                             input$UF_selecionada_corretora,
+                                             ".rds"))
+    
+    periodo_futuro <- make_future_dataframe(modelo_corretora, periods = input$periodo_selecionado * 12, freq = "month")
+    
+    projecao <- predict(modelo_corretora, periodo_futuro)
+    
+    plot_corretora <- dyplot.prophet(modelo_corretora, projecao, uncertainty = TRUE, 
+                                          main = paste0("Projeção de Abertura de Corretoras - ", input$UF_selecionada_corretora),
+                                          xlab = "Linha Temporal",
+                                          ylab = "Quantidade de Corretoras")
+    
+    return(plot_corretora)
+    
+  })
+  
+  output$PLOT_ABERTURA_CORRETORAS_UF <- renderDygraph(renderiza_plot_corretoras())
+  
   }
 
 # Run the application 
--- PreparoDadosInteligencia.R
+++ PreparoDadosInteligencia.R
@@ -1,10 +1,9 @@
 library(shiny)
-library(ggplot2)
+library(prophet)
 library(shinydashboard)
-library(htmltools)
+library(shinyWidgets)
+library(dygraphs)
 library(readr)
-library(dplyr)
-library(tidyr)
 
 options(warn = -1)
 options(scipen = 999999)
@@ -13,56 +12,5 @@ DADOS_RAMOS <- read_delim("NOMES_RAMOS.CSV",
                           delim = ",", escape_double = FALSE, 
                         trim_ws = TRUE)
 
-DADOS_REGIOES <- read_delim("regioes_tarifacao.csv", 
-                            delim = ",", escape_double = FALSE, 
-                            trim_ws = TRUE)
-
-SCORE_INADIMPLENCIA <- read_delim("www/SCORE_INADIMPLENTES/SCORE_INADIMPLENCIA.csv", 
-                            delim = ",", escape_double = FALSE, 
-                            trim_ws = TRUE)
-
-NOMES_CORRETORAS <- read_delim("www/CONSUMO_CORRETORAS/nome_corretoras.csv", 
-                               delim = ",", escape_double = FALSE, 
-                               trim_ws = TRUE)
-
-NOMES_CORRETORAS$cod_corretora <- substring(NOMES_CORRETORAS$nome_corretora, 0, 4)
-
-COMENTARIOS_CLASSIFICADOS <- read_delim("www/CLASSIFICADOR_NPS/comentarios_classificados.csv", 
-                                        delim = ",", escape_double = FALSE, 
-                                        trim_ws = TRUE)
-
-RESUMO_CLASSIFICACAO <- read_delim("www/CLASSIFICADOR_NPS/resumo_compilados.csv", 
-                                   delim = ",", escape_double = FALSE, 
-                                   trim_ws = TRUE)
-
-RESUMO_FREQUENCIA <- read_delim("www/CLASSIFICADOR_NPS/resumo_frequencia.csv", 
-                                   delim = ",", escape_double = FALSE, 
-                                   trim_ws = TRUE)
-
-lista_corretoras <- as.factor(NOMES_CORRETORAS$nome_corretora)
-
-TOP10_INADIMPLENTES_CLIENTES <- SCORE_INADIMPLENCIA %>% 
-  filter(cliente_regula == 1) %>%
-  top_n(chance_inadimplencia, n = 10) %>%
-  arrange(desc(chance_inadimplencia)) %>%
-  select(raiz_cnpj, unidade, dv, razao_social, chance_inadimplencia)
-
-POSSIVEIS_INADIMPLENTES_MERCADO <- SCORE_INADIMPLENCIA %>%
-  filter(cliente_regula == 0) %>%
-  arrange(desc(chance_inadimplencia)) %>%
-  select(raiz_cnpj, unidade, dv, razao_social, chance_inadimplencia)
-
-DADOS_RAMOS$coramo <- as.numeric(DADOS_RAMOS$coramo)
-
-lista_ramos <- data.frame(table(DADOS_RAMOS$nome_ramo))
-lista_ramos <- as.factor(lista_ramos$Var1[lista_ramos$Freq != ""])
-
-lista_UF <- as.factor(c("AC", "AL", "AM", "BA", "CE",
-                      "DF", "ES", "GO", "MA", "MG", 
-                      "MS", "MT", "PA", "PB", "PE",
-                      "PI", "PR", "RJ", "RN", "RO",
-                      "RR","RS", "SC", "SE", "SP", 
-                      "TO"))
-
-lista_regioes <- as.factor(DADOS_REGIOES$regiao)
+DADOS_RAMOS <- DADOS_RAMOS[DADOS_RAMOS$escolhido == 1, ]
 
